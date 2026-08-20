a3d061c58299adcdcc4b41c11dbb5d6af6beb643
Hokubu Subway <hokubusub@gmail.com>
2026-08-20 20:46:06 +0900

    getUser を定義

--- backend/internal/handler/user.go
+++ backend/internal/handler/user.go
@@ -12,6 +12,10 @@ import (
 
 type UserHandler struct{}
 
+type UserQuery struct {
+	ID uuid.UUID `json:"id"`
+}
+
 type UserResponse struct {
 	ID     uuid.UUID `json:"id"`
 	UserID string    `json:"userId"`
@@ -22,15 +26,52 @@ func NewUserHandler() *UserHandler {
 	return &UserHandler{}
 }
 
+// UserID ユーザーの ID
+type UserID = string
+
+// UserName ユーザー名
+type UserName = string
+
+// Count 自然数
+type Count = int
+
+// UserProfile defines model for UserProfile.
+type UserProfile struct {
+	// ID UUID
+	ID *uuid.UUID `json:"id,omitempty"`
+
+	// MessageCount 自然数
+	MessageCount *Count `json:"messageCount,omitempty"`
+
+	// Name ユーザー名
+	Name *UserName `json:"name,omitempty"`
+
+	// StampCount 自然数
+	StampCount *Count `json:"stampCount,omitempty"`
+
+	// UserID ユーザーの ID
+	UserID UserID `json:"userId"`
+}
+
 func (h *UserHandler) GetMe(c echo.Context) error {
 	user, ok := authmiddleware.GetAuthenticatedUser(c)
 	if !ok {
 		return c.NoContent(http.StatusUnauthorized)
 	}
-
 	return c.JSON(http.StatusOK, UserResponse{
 		UserID: user.UserId,
 		Name:   user.Name,
 		ID:     user.Id,
+	})
+}
+
+func (h *UserHandler) GetUser(c echo.Context) error {
+	var user UserQuery
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	return c.JSON(http.StatusOK, UserProfile{
+		ID: &(user.ID),
+		// uuid から traQ で API たたきますよ
 	})
 }
--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -38,6 +38,6 @@ func registerRoutes(e *echo.Echo) {
 	api := e.Group("/api")
 	api.Use(authmiddleware.ForwardedUser)
 	api.GET("/users/me", userHandler.GetMe)
+	api.GET("/users/:userId", userHandler.GetUser)
 	api.POST("/oauth/token", handler.OAuth)
-
 }
