f160adec43a6168174fa9146c0e2d232282cd622
Hokubu Subway <hokubusub@gmail.com>
2026-08-23 00:13:20 +0900

    GetTimeline 作った

--- backend/internal/handler/timeline.go
+++ backend/internal/handler/timeline.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
+	"sort"
+	"time"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,13 +16,33 @@ func NewTimelineHandler() *TimelineHandler {
 	return &TimelineHandler{}
 }
 
-func GetActivity(sbp, all string) (error) {
+type TimelineReceived struct {
+	MessageID uuid.UUID `json:"id"`
+	UserID uuid.UUID `json:"userId"`
+	ChannelID uuid.UUID `json:"channelId"`
+	Content string `json:"content"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type TimelineResponse struct {
+	Messages []uuid.UUID `json:"messages"`
+}
+
+func GetActivity(sbp, all string) (*[]TimelineReceived, error) {
 	result, err := http.NewRequest("GET", "https://q.trap.jp/api/v3/activity/timeline?all=" + all, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer result.Body.Close()
-	return nil
+	var res []TimelineReceived
+	json.NewDecoder(result.Body).Decode(&res)
+	if sbp == "true" {
+		sort.Slice(res, func(i, j int) bool {
+			return res[i].CreatedAt.After(res[j].CreatedAt)
+		})
+	}
+	return &res, nil
 }
 
 func (h *TimelineHandler) GetTimeline(c echo.Context) error {
@@ -26,10 +50,17 @@ func (h *TimelineHandler) GetTimeline(c echo.Context) error {
 	if !(params.Has("sortByPopularity")||params.Has("all")) {
 		return c.JSON(http.StatusBadRequest, nil)
 	}
-	if params.Get("sortByPopularity") == "true" {
-		// sort
+	res, err := GetActivity(params.Get("sortByPopularity"), params.Get("all"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
 	}
-	return nil
+	result := make([]uuid.UUID, len(*res))
+	for i, v := range *res {
+		result[i] = v.MessageID
+	}
+	return c.JSON(http.StatusOK, TimelineResponse {
+		Messages: result,
+	})
 }
 
 func (H *TimelineHandler) GetIn(c echo.Context) error {
