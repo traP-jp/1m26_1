464b510bd4c059b8c46d5bd27989e166e0cd166c
Hokubu Subway <hokubusub@gmail.com>
2026-08-22 23:59:21 +0900

    GetActivity

--- backend/internal/handler/timeline.go
+++ backend/internal/handler/timeline.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
+	"google.golang.org/genproto/googleapis/type/datetime"
 )
 
 type TimelineHandler struct {}
@@ -12,13 +15,24 @@ func NewTimelineHandler() *TimelineHandler {
 	return &TimelineHandler{}
 }
 
-func GetActivity(sbp, all string) (error) {
+type TimelineReceived struct {
+	MessageID uuid.UUID `json:"id"`
+	UserID uuid.UUID `json:"userId"`
+	ChannelID uuid.UUID `json:"channelId"`
+	Content string `json:"content"`
+	CreatedAt datetime.DateTime `json:"createdAt"`
+	UpdatedAt datetime.DateTime `json:"updatedAt"`
+}
+
+func GetActivity(sbp, all string) (*[]TimelineReceived, error) {
 	result, err := http.NewRequest("GET", "https://q.trap.jp/api/v3/activity/timeline?all=" + all, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer result.Body.Close()
-	return nil
+	var res []TimelineReceived
+	json.NewDecoder(result.Body).Decode(&res)
+	return &res, nil
 }
 
 func (h *TimelineHandler) GetTimeline(c echo.Context) error {
