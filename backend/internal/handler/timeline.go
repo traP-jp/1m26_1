f183439c6ff42fd9e975ef6dc5cb2e862888a92e
Hokubu Subway <hokubusub@gmail.com>
2026-08-23 19:06:16 +0900

    API のリクエスト先を切り替えました

--- backend/internal/handler/timeline.go
+++ backend/internal/handler/timeline.go
@@ -171,7 +171,7 @@ func GetStamps(id string) (*Stamps, error) {
 
 func (h *TimelineHandler) GetTimeline(c echo.Context) error {
 	params := c.QueryParams()
-	if !(params.Has("sortByPopularity") || params.Has("all")) {
+	if !(params.Has("sortByPopularity")) {
 		return c.JSON(http.StatusBadRequest, nil)
 	}
 	res, _, bm, err := GetActivity(params.Get("sortByPopularity"), "true", "before="+h.bottomMessage.String())
